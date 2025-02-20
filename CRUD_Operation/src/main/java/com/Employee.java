eee0a7beada71895bb09366f73e2308061195e34
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-02-20 18:08:03 +0530

    EmployeeDetails

---
+++ CRUD_Operation/src/main/java/com/Employee.java
@@ -0,0 +1,47 @@
+package com;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+	
+	@Id
+	private int id;
+	private String name;
+	private String job;
+	private double sal;
+	private long phoneNo;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public double getSal() {
+		return sal;
+	}
+	public void setSal(double sal) {
+		this.sal = sal;
+	}
+	public long getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(long phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	
+
+}
