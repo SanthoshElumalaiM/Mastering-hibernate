9cc2d2add58d4e0d1a4c85301dba593d850bd5e9
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-02-21 15:31:05 +0530

    Save

--- CRUD_Operation/src/main/java/com/SaveEmployee.java
+++ CRUD_Operation/src/main/java/com/SaveEmployee.java
@@ -12,19 +12,19 @@ public class SaveEmployee
 	{
 		
 		Employee e=new Employee();
-		e.setId(1);
-		e.setName("Sant");
-		e.setJob("Developer");
-		e.setSal(100020);
-		e.setPhoneNo(8983);
+		e.setId(2);
+		e.setName("Hardik");
+		e.setJob("All-Rounder");
+		e.setSal(20000);
+		e.setPhoneNo(789373);
 		
 		EntityManagerFactory emf=Persistence.createEntityManagerFactory("dev");
 		EntityManager em=emf.createEntityManager();
 		EntityTransaction et=em.getTransaction();
 		
 		et.begin();
-//		em.persist(e);
-		em.merge(e);
+		em.persist(e);
+//		em.merge(e);
 			
 		et.commit();
 		System.out.println("data saved");
