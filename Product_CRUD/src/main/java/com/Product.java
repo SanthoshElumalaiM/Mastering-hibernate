2ec0ae08531901085c1e5ddbab4fdd5b95acbf8f
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-02-21 15:53:19 +0530

    AddProduct

---
+++ Product_CRUD/src/main/java/com/Product.java
@@ -0,0 +1,54 @@
+package com;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+	@Id
+	private int product_id;
+	private String product_name;
+	private String brand;
+	private double price;
+	private String colour;
+	private int quantity;
+	public int getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+	public String getProduct_name() {
+		return product_name;
+	}
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getColour() {
+		return colour;
+	}
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+}
