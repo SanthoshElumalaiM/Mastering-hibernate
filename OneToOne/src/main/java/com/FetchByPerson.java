dbee9e5e84541a16e497e1d4fa1cf616e2d97978
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-02-26 16:12:43 +0530

    FetchByPerson

---
+++ OneToOne/src/main/java/com/FetchByPerson.java
@@ -0,0 +1,25 @@
+package com;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class FetchByPerson {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf=Persistence.createEntityManagerFactory("dev");
+		EntityManager em=emf.createEntityManager();
+		EntityTransaction et=em.getTransaction();
+		
+		
+		Person p=em.find(Person.class, 1);
+		p.setAddress(null);
+		et.begin();
+		em.merge(p);
+		et.commit();
+		
+		System.out.println(p);
+	}
+
+}
