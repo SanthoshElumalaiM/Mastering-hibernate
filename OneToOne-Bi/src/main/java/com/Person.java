dd50717490947ff53af286a32eca5cceadf2ec39
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-02-27 15:48:03 +0530

    ToString

--- OneToOne-Bi/src/main/java/com/Person.java
+++ OneToOne-Bi/src/main/java/com/Person.java
@@ -21,4 +21,60 @@ public class Person {
 	
 	@OneToOne
 	private AadharCard aadharCard;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(long phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public AadharCard getAadharCard() {
+		return aadharCard;
+	}
+
+	public void setAadharCard(AadharCard aadharCard) {
+		this.aadharCard = aadharCard;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", gender=" + gender + ", email=" + email + ", phoneNo="
+				+ phoneNo + "]";
+	}
+	
+	
 }
