6d61da04bfb732b416fd322640e6e5996f9b2004
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-02-27 15:47:27 +0530

    ToString

--- OneToOne-Bi/src/main/java/com/AadharCard.java
+++ OneToOne-Bi/src/main/java/com/AadharCard.java
@@ -10,7 +10,7 @@ import jakarta.persistence.OneToOne;
 public class AadharCard {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
 	private long ano;
 	private String fatherName;
 	private String address;
@@ -18,4 +18,50 @@ public class AadharCard {
 	
 	@OneToOne
 	private Person person;
+
+	public long getAno() {
+		return ano;
+	}
+
+	public void setAno(long ano) {
+		this.ano = ano;
+	}
+
+	public String getFatherName() {
+		return fatherName;
+	}
+
+	public void setFatherName(String fatherName) {
+		this.fatherName = fatherName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+
+	@Override
+	public String toString() {
+		return "AadharCard [ano=" + ano + ", fatherName=" + fatherName + ", address=" + address + ", dob=" + dob + "]";
+	}
+	
 }
