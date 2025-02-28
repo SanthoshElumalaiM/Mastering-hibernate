a599a02247f731753279be1ebb2310cb5fa7b3c5
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-02-28 16:11:48 +0530

    Dept

---
+++ OntToMany/src/main/java/com/Department.java
@@ -0,0 +1,51 @@
+package com;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Department {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String deptName;
+	private String hod;
+	private int noOfStudents;
+	private double fee;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDeptName() {
+		return deptName;
+	}
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+	public String getHod() {
+		return hod;
+	}
+	public void setHod(String hod) {
+		this.hod = hod;
+	}
+	public int getNoOfStudents() {
+		return noOfStudents;
+	}
+	public void setNoOfStudents(int noOfStudents) {
+		this.noOfStudents = noOfStudents;
+	}
+	public double getFee() {
+		return fee;
+	}
+	public void setFee(double fee) {
+		this.fee = fee;
+	}
+	
+	
+}
