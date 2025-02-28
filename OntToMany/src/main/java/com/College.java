50ed1151e2660401c3894eccd2b85b9e60bfec58
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-02-28 16:11:36 +0530

    OneToMany

---
+++ OntToMany/src/main/java/com/College.java
@@ -0,0 +1,75 @@
+package com;
+
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+@Entity
+public class College {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String CollegeName;
+	private String location;
+	private String email;
+	private long contact;
+	
+	@OneToMany
+	private List<Department> departments;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCollegeName() {
+		return CollegeName;
+	}
+
+	public void setCollegeName(String collegeName) {
+		CollegeName = collegeName;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getContact() {
+		return contact;
+	}
+
+	public void setContact(long contact) {
+		this.contact = contact;
+	}
+
+	public List<Department> getDepartments() {
+		return departments;
+	}
+
+	public void setDepartments(List<Department> departments) {
+		this.departments = departments;
+	}
+	
+	
+
+}
