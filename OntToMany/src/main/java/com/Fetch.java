5be823b31dc983cc35767856d0002617895f2987
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-01 15:23:40 +0530

    Fetch

---
+++ OntToMany/src/main/java/com/Fetch.java
@@ -0,0 +1,26 @@
+package com;
+
+import java.util.List;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Fetch {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf=Persistence.createEntityManagerFactory("dev");
+		EntityManager em=emf.createEntityManager();
+		
+		College c=em.find(College.class, 1);
+		System.out.println(c);
+		
+		List<Department> list=c.getDepartments();
+		for(Department d: list)
+		{
+			System.out.println(d);
+		}
+	}
+
+}
