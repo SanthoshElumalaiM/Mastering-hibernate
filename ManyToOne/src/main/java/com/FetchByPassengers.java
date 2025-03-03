f8cc7746bcaf385e94d2abb200b205c8962778af
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-03 15:33:48 +0530

    FetchByPassen

---
+++ ManyToOne/src/main/java/com/FetchByPassengers.java
@@ -0,0 +1,19 @@
+package com;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class FetchByPassengers {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf=Persistence.createEntityManagerFactory("dev");
+		EntityManager em=emf.createEntityManager();
+		
+		Passengers p=em.find(Passengers.class, 1);
+		
+		System.out.println(p);
+
+	}
+
+}
