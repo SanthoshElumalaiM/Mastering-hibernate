3e3961edc539e73382bc5747a5be409d65c1b880
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-04 15:23:43 +0530

    GetterSetter

--- OneToMany-Bi/src/main/java/com/Batch.java
+++ OneToMany-Bi/src/main/java/com/Batch.java
@@ -16,10 +16,68 @@ public class Batch {
 	private int id;
 	private String bcode;
 	private String subject;
+	private String trainer;
 	private String timing;
 	private int noOfStudents;
 	
 	@OneToMany
 	private List< Student> students;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getBcode() {
+		return bcode;
+	}
+
+	public void setBcode(String bcode) {
+		this.bcode = bcode;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getTiming() {
+		return timing;
+	}
+
+	public void setTiming(String timing) {
+		this.timing = timing;
+	}
+
+	public int getNoOfStudents() {
+		return noOfStudents;
+	}
+
+	public void setNoOfStudents(int noOfStudents) {
+		this.noOfStudents = noOfStudents;
+	}
+
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public String getTrainer() {
+		return trainer;
+	}
+
+	public void setTrainer(String trainer) {
+		this.trainer = trainer;
+	}
+
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+	
 
 }
