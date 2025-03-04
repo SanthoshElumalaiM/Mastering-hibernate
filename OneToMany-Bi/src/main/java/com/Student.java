a5dab4b7552ed84d2ba7aa0e7830e39c97f163ca
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-04 15:36:14 +0530

    @joinColumn

--- OneToMany-Bi/src/main/java/com/Student.java
+++ OneToMany-Bi/src/main/java/com/Student.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
@@ -19,6 +20,7 @@ public class Student {
 	private long phoneNo;
 	
 	@ManyToOne
+	@JoinColumn
 	private Batch batch;
 
 	public int getId() {
