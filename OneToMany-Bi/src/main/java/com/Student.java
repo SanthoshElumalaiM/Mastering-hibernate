f57e4bccc7dc6219b9707ace94d2dcd65f87f796
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-04 15:23:55 +0530

    GetterSetter

--- OneToMany-Bi/src/main/java/com/Student.java
+++ OneToMany-Bi/src/main/java/com/Student.java
@@ -13,13 +13,70 @@ public class Student {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
-	private String age;
+	private int age;
 	private String gender;
 	private String email;
 	private long phoneNo;
 	
 	@ManyToOne
 	private Batch batch;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(long phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public Batch getBatch() {
+		return batch;
+	}
+
+	public void setBatch(Batch batch) {
+		this.batch = batch;
+	}
+	
 	
 
 }
