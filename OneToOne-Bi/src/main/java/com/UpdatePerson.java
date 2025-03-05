f7ee5f766e5dad7308fd4da6ceec9156a8a10f95
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-05 16:08:29 +0530

    update

---
+++ OneToOne-Bi/src/main/java/com/UpdatePerson.java
@@ -0,0 +1,29 @@
+package com;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class UpdatePerson {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf=Persistence.createEntityManagerFactory("dev");
+		EntityManager em=emf.createEntityManager();
+		EntityTransaction et=em.getTransaction();
+		
+		Person p=em.find(Person.class, 1);
+		if(p!=null)
+		{
+			p.setName("Santhosh");
+			et.begin();
+			em.merge(p);
+			et.commit();
+			System.out.println("Data updated");
+		}
+		else {
+			System.out.println("Data is Not found");
+		}
+	}
+
+}
